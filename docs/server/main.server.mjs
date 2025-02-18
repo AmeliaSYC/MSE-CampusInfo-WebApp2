7d0a5055fddf7830f7cf9d794365f0643c19da2b
Amelia <ameliawestwood@gmail.com>
2025-02-18 02:34:55 +0100

    update

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-4YZMAMOY.mjs";import"./chunk-I7O65H32.mjs";import"./chunk-LY6ZSRI4.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ScheduleComponent } from './schedule/schedule.component';
-import { LocationComponent } from './location/location.component';
-
-export const routes: Routes = [
-    { path: 'schedule', component: ScheduleComponent },
-    { path: 'location', component: LocationComponent },
-    { path: '', redirectTo: '/schedule', pathMatch: 'full'},
-];
